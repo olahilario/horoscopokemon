59c529499e4343b35ee99fa0e4f3b19f61e89501
olahilario <grandejacare@gmail.com>
2024-08-18 18:27:54 -0300

    updated

--- serious_horoscopokemon/src/components/RandomBar.jsx
+++ serious_horoscopokemon/src/components/RandomBar.jsx
@@ -2,6 +2,7 @@ import React, {useState, createContext, useContext} from 'react'
 import { SearchContext } from '../contexts/searchContext'
 import styles from './RandomBar.module.css'
 import { Link } from 'react-router-dom'
+import lupa from '../assets/search_pokeball_icon.jpg'
 
 
 
@@ -19,7 +20,7 @@ function RandomBar() {
   return (
     <div>
        <div className={styles.search_bar}>
-        <img src='/src/assets/search_pokeball_icon.webp' className={styles.lupa} alt="search icon" />
+        <img src={lupa} className={styles.lupa} alt="search icon" />
         <input type="text" id='randombar' className={styles.randombar} onChange={handleAnything} placeholder="Type anything you want. It doesn't matter, because your horoscopoke does! Simple rule: Space controls what is true or false! ᕙ(⇀‸↼‶)ᕗ"/>
         <button><Link to='/Conclusion'><span>GO!</span></Link></button>
       </div>
